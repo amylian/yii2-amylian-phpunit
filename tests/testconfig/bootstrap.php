1e6dd8fff35001549ee6023b944768450ed29386
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-28 00:18:21 +0200

    fixed tests-bootstrap vendor dir detection

--- tests/testconfig/bootstrap.php
+++ tests/testconfig/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
-define ('ABEXTO_AMYLIAN_VENDOR_PATH' , __DIR__.'/../../../..');
-require_once ABEXTO_AMYLIAN_VENDOR_PATH.'/autoload.php';
+$autoLoader = __DIR__.'/../../../../autoload.php';
+if (file_exists($autoLoader)) {
+    require_once $autoLoader;
+} else {
+    $autoLoader = __DIR__.'/../../vendor/autoload.php';
+    require_once $autoLoader;
+}
+define ('ABEXTO_AMYLIAN_VENDOR_PATH' , dirname($autoLoader));
 abexto\amylian\yii\phpunit\Bootstrap::initEnv(__FILE__, __DIR__.'/..');
 
